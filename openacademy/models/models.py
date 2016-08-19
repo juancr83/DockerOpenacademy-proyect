894cb7dfd1cf39573bca84c64a12c3c64fa02e3c
Juan Carlos Rivera <juancr83@outlook.com>
2016-08-19 22:02:09 +0000

    [REF] update

--- openacademy/models/models.py
+++ openacademy/models/models.py
@@ -5,8 +5,8 @@ from openerp import models, fields, api
 class Course(models.Model):
     _name = 'course'
 
-    name = fields.Char()
-    description = fields.Text()
+    name = fields.Char(string='Title', required=True)
+    description = fields.Text(string='Descripcion')
     responsible = fields.Many2one('res.users',
 				ondelete='set null',
 				string="Responsible", index=True)
